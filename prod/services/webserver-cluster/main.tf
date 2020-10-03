63644239367f2f6bfef3d859f911ac83dd4faaaf
Fionn Fitzmaurice <fionn@github.com>
2020-10-03 15:20:57 +0800

    Update to enable new user data

--- prod/services/webserver-cluster/main.tf
+++ prod/services/webserver-cluster/main.tf
@@ -1,14 +1,15 @@
 module "webserver_cluster" {
-  source = "github.com/fionn/tfuar-modules//services/webserver-cluster?ref=v0.0.3"
+  source = "github.com/fionn/tfuar-modules//services/webserver-cluster?ref=v0.0.5"
 
   cluster_name           = "webservers-prod"
   db_remote_state_bucket = "terraform-state-tfuar"
   db_remote_state_key    = "prod/data-stores/mysql/terraform.tfstate"
 
-  instance_type      = "t2.micro"
-  min_size           = 2
-  max_size           = 10
-  enable_autoscaling = true
+  instance_type        = "t2.micro"
+  min_size             = 2
+  max_size             = 10
+  enable_autoscaling   = true
+  enable_new_user_data = false
 
   custom_tags = {
     Owner      = "team-foo"
