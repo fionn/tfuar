9296d68a599f30c345844c45cf58adab6dd3d7d6
Fionn Fitzmaurice <fionn@github.com>
2020-10-03 16:24:25 +0800

    Zero downtime

--- staging/services/webserver-cluster/main.tf
+++ staging/services/webserver-cluster/main.tf
@@ -1,15 +1,17 @@
 module "webserver_cluster" {
-  source = "github.com/fionn/tfuar-modules//services/webserver-cluster?ref=v0.0.5"
+  source = "github.com/fionn/tfuar-modules//services/webserver-cluster?ref=v0.0.6"
+
+  ami         = "ami-06f0013cbb22c8dd3"
+  server_text = "foo bar"
 
   cluster_name           = "webservers-staging"
   db_remote_state_bucket = "terraform-state-tfuar"
   db_remote_state_key    = "staging/data-stores/mysql/terraform.tfstate"
 
-  instance_type        = "t2.micro"
-  min_size             = 2
-  max_size             = 2
-  enable_autoscaling   = true
-  enable_new_user_data = true
+  instance_type      = "t2.micro"
+  min_size           = 2
+  max_size           = 2
+  enable_autoscaling = true
 }
 
 resource "aws_security_group_rule" "allow_testing_inbound" {
